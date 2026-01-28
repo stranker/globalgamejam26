c95462e9e6c321635ee0551c9417f31208683315
Daniel <dani_mrx93@hotmail.com>
2026-01-28 18:10:08 -0300

    Add connected_dots minigame

---
+++ officemasks/scenes/levels/minigames/connect_dots/connect_grid_game_scene.gd
@@ -0,0 +1,32 @@
+extends Control
+
+@onready var connect_grid: ConnectGrid = $Panel/GamePivot/ConnectGrid
+@onready var tries_label: Label = $Panel/Tries
+@export var tries: int
+@onready var reset_button: Button = $Panel/Reset
+
+signal win()
+signal lose()
+
+func _ready() -> void:
+	tries_label.text = "Tries:" + str(tries)
+	connect_grid.game_win.connect(on_game_win)
+	pass
+
+func _on_reset_button_down() -> void:
+	tries -= 1
+	update_tries_label()
+	if tries > 0:
+		connect_grid.reset_game()
+	else:
+		lose.emit()
+	pass # Replace with function body.
+
+func update_tries_label():
+	tries_label.text = "Tries:" + str(tries)
+	pass
+
+func on_game_win():
+	win.emit()
+	reset_button.hide()
+	pass
