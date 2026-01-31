e0904158804e4f32c3273a8f85a9000dd0cbe9b0
Tomas Ayala <tomasayala01@gmail.com>
2026-01-31 15:56:43 -0300

    Adding signals to the objecto_test

--- officemasks/objects/test/objecto_test.gd
+++ officemasks/objects/test/objecto_test.gd
@@ -3,6 +3,9 @@ extends Area2D
 @onready var interaction_area = $InteractionArea
 @onready var sprite_2d: Sprite2D = $Sprite2D
 
+signal dialog_started
+signal dialog_ended
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	interaction_area.interact = Callable(self, "_on_jijear")
@@ -18,10 +21,11 @@ func _on_jijear():
 		#Global.objeto_is_basado = false
 	else:
 		sprite_2d.texture = preload("uid://dp30vhwtxp064")
-		#Global.objeto_is_basado = true
 
 func on_dialog_started(_dialog):
+	dialog_started.emit()
 	CinematicCamera.target_node(self, 0.5)
 
 func on_dialog_ended(_dialog):
+	dialog_ended.emit()
 	CinematicCamera.reset()
