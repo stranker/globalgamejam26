d946e2bd1bbc4b8c9f733ec27497100c87954a4a
Tomas Ayala <tomasayala01@gmail.com>
2026-01-31 11:48:01 -0300

    Interaction manager and example classes

---
+++ officemasks/objects/test/objecto_basado.gd
@@ -0,0 +1,26 @@
+extends Area2D
+
+@onready var interaction_area = $InteractionArea
+@onready var sprite_2d: Sprite2D = $Sprite2D
+#export var _is_basado = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	interaction_area.interact = Callable(self, "_on_jijear")
+	DialogueManager.dialogue_started.connect(on_dialog_started)
+	#DialogueManager.dialogue_ended.connect(on_dialog_ended)
+
+
+func _on_jijear():
+	CinematicCamera.target_node(self, 0.5)
+	DialogueManager.show_dialogue_balloon(preload("uid://co8swqfuvjel"))
+	
+	if Global.objeto_is_basado:
+		sprite_2d.texture = preload("uid://vo6n0eugmiyq")
+		Global.objeto_is_basado = false
+	else:
+		sprite_2d.texture = preload("uid://dp30vhwtxp064")
+		Global.objeto_is_basado = true
+
+func on_dialog_started(dialog):
+	CinematicCamera.target_node(self, 0.5)
