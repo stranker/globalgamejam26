4c25bf61b83a56cb2b7028e5099c8570f00eccf4
Tomas Ayala <tomasayala01@gmail.com>
2026-01-31 19:18:20 -0300

    Blink and moving objects

---
+++ officemasks/scenes/levels/minigames/desk_point_and_click/blink.gd
@@ -0,0 +1,28 @@
+extends CanvasLayer
+
+@onready var parpado_superior := $ParpadoSuperior
+@onready var parpado_inferior := $ParpadoInferior
+@onready var animation_player := $AnimationPlayer
+
+var current_desk_obj : DeskObject
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	parpado_inferior.visible = false
+	parpado_superior.visible = false
+	animation_player.animation_finished.connect(_on_blink_finished)
+	pass # Replace with function body.
+
+func blink(desk_obj : DeskObject):
+	parpado_inferior.visible = true
+	parpado_superior.visible = true
+	animation_player.play("blink")
+	current_desk_obj = desk_obj
+	
+func _on_blink_finished(_animation_name):
+	parpado_inferior.visible = false
+	parpado_superior.visible = false
+	
+	
+func _move_object_in_blink():
+	current_desk_obj.move_object()
--- officemasks/scenes/levels/minigames/desk_point_and_click/desk_object.gd
+++ officemasks/scenes/levels/minigames/desk_point_and_click/desk_object.gd
@@ -1,18 +1,28 @@
+@tool
 extends Area2D
+class_name DeskObject
 
 @export var linked_object : Area2D
+@onready var sprite_2d := $Sprite2D
+@export var texture : Texture:
+	set(new_texture):
+		texture = new_texture
+		if sprite_2d:
+			sprite_2d.texture = new_texture
 var is_highlighted : bool 
+
+signal blink
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	is_highlighted = false
+	sprite_2d.texture = texture
 	if linked_object != null:
 		linked_object.hide()
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed("mouse_action") && is_highlighted && linked_object != null:
-		linked_object.visible = true
-		self.visible = false
+		blink.emit()
 
 func _on_mouse_entered() -> void:
 	is_highlighted = true
@@ -21,3 +31,7 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	is_highlighted = false
 	pass # Replace with function body.
+
+func move_object():
+	linked_object.visible = true
+	self.visible = false
