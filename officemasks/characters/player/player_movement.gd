a610f1b545b7b44995a5c62e161a47f523da681e
Tomas Ayala <tomasayala01@gmail.com>
2026-02-01 17:53:52 -0300

    Adding changing the mask as the global score progresses

--- officemasks/characters/player/player_movement.gd
+++ officemasks/characters/player/player_movement.gd
@@ -3,11 +3,15 @@ class_name Player
 
 @export var movement_speed : float = 100
 @export var enable_y: bool = true
+@onready var mask: Sprite2D = $Mask
 var character_direction : Vector2
+var current_mask_score : int
+var mask_textures : Array[Texture] = [preload("uid://chp6ut3m7ur5n"), preload("uid://4mvpumrpixyj"), preload("uid://cegd753eeoiv6"), preload("uid://cjb30ibov5ayw"), preload("uid://2dp85w7vjn4r")]
 
 func _ready() -> void:
 	DialogueManager.dialogue_started.connect(_on_dialog_start)
 	DialogueManager.dialogue_ended.connect(_on_dialog_ended)
+	current_mask_score = 0
 	pass
 
 func _physics_process(delta):
@@ -35,5 +39,8 @@ func _on_dialog_start(dialogue):
 	pass
 
 func _on_dialog_ended(dialogue):
+	if current_mask_score < Global.broken_mask_score && Global.broken_mask_score < mask_textures.size():
+		current_mask_score = Global.broken_mask_score
+		mask.texture = mask_textures[current_mask_score]
 	set_physics_process(true)
 	pass
