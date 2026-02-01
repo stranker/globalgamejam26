30ece956cc14881c5479fbb68e68ff4f345a0806
Tomas Ayala <tomasayala01@gmail.com>
2026-02-01 17:27:52 -0300

    Adding the animations for the breakage

---
+++ officemasks/scenes/ui/mask.gd
@@ -0,0 +1,14 @@
+extends CanvasLayer
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _ready() -> void:
+	animation_player.animation_finished.connect(_on_break_finished)
+
+func mask_break():
+	self.visible = true
+	var animation = "break_" + str(Global.broken_mask_score +1)
+	animation_player.play(animation)
+	
+func _on_break_finished(_animation_name):
+	self.visible = false
