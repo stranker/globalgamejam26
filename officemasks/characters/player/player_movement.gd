818987d09a4f1ca4329133c196d944ed22d4c855
SantiGG04 <gandolfo.santiago@gmail.com>
2026-02-01 18:29:22 -0300

    Sonido de Pasos

--- officemasks/characters/player/player_movement.gd
+++ officemasks/characters/player/player_movement.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 class_name Player
 
+@onready var footsteps: AudioStreamPlayer2D = $Footsteps
 @export var movement_speed : float = 100
 @export var enable_y: bool = true
 var character_direction : Vector2
@@ -23,14 +24,20 @@ func _physics_process(delta):
 	if character_direction:
 		velocity = character_direction * movement_speed
 		if %Sprite.animation != "Walking": %Sprite.animation = "Walking"
+		if not footsteps.playing:
+			footsteps.play()
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, movement_speed)
 		if %Sprite.animation != "Idle": %Sprite.animation = "Idle"
+		if footsteps.playing:
+			footsteps.stop()
 		
 	move_and_slide()
 	pass
 
 func _on_dialog_start(dialogue):
+	if footsteps.playing:
+		footsteps.stop()
 	set_physics_process(false)
 	pass
 
